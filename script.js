3e59a9cccaa614d57dae5d3cf908ef08d76b1405
Thomas Gorissen <mail@serrynaimo.com>
2015-10-27 00:59:02 +0800

    intro

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 var blockBuzz = true,
-    answerValue = 0;
+    answerValue = 0,
+    introClick = 1;
 
 var onKeyPress = function(e) {
     if(e.keyCode > 48 && e.keyCode < 52) {
         triggerPlayer(e.keyCode - 48);
     }
+};
+
+var onDocumentClick = function(e) {
+    
+    if(introClick == 1) {
+        Array.prototype.forEach.call(document.querySelectorAll("#players input"), function(input) {
+            input.readOnly = true;
+        });
+    }
+    
+    if(introClick < 7) {       
+        var elem = document.getElementById("column" + introClick).children[0];
+        
+        if(elem.classList.contains("highlight")) {
+            var x = elem.offsetLeft + elem.offsetWidth / 2;
+            var y = elem.offsetTop - elem.offsetHeight / 2;
+            elem.children[1].style.transformOrigin = x + "px " + y + "px 0px";
+            elem.classList.remove('highlight');
+            elem.classList.add('visible');
+            introClick++; 
+        }
+        else {
+            document.getElementById("column" + introClick).children[0].classList.add('highlight');
+        }
+    }
+    
+    if(introClick == 7) {
+        setTimeout(function() {
+            document.getElementById("board").classList.add("start");
+        }, 1500);
+    }
+
 };
 
 var onPlayerClick = function(e) {    
@@ -31,7 +64,7 @@ var onCardClick = function(e) {
        e.currentTarget.classList.remove("show")
        e.currentTarget.classList.add("done");     
     }
-    else {
+    else if(introClick >= 7) {
         var x = e.currentTarget.offsetLeft + e.currentTarget.offsetWidth / 2;
         var y = e.currentTarget.offsetTop + e.currentTarget.offsetHeight / 2;
         e.currentTarget.children[2].style.transformOrigin = x + "px " + y + "px 0px";
@@ -44,10 +77,6 @@ var onCardClick = function(e) {
         }
         catch(e) {}
         }
-    
-    Array.prototype.forEach.call(document.querySelectorAll("#players input"), function(input) {
-        input.readOnly = true;
-    });
 };
 
 var triggerPlayer = function(player_number) { // player_numbers: 1, 2, 3
@@ -60,6 +89,7 @@ var triggerPlayer = function(player_number) { // player_numbers: 1, 2, 3
 }
 
 document.onkeypress = onKeyPress;
+document.getElementById("board").onclick = onDocumentClick;
 
 Array.prototype.forEach.call(document.getElementById("players").childNodes, function(player) {
     player.onclick = onPlayerClick;
